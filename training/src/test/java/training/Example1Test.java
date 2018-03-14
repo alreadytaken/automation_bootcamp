c6671436f6af645b7eac4ab53dce0131bccbd8a7
nano02 <fcmont98@gmail.com>
2018-03-14 01:09:35 +0100

    Edit

--- training/src/test/java/training/Example1Test.java
+++ training/src/test/java/training/Example1Test.java
@@ -1,5 +1,8 @@
 package training;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -11,21 +14,22 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Example1Test {
+	private static final Logger LOG = LogManager.getLogger(Example1Test.class);
 	@BeforeClass
 	public void BeforeClass() {
-		System.out.println("Principio");
+		LOG.info("Principio");
 	}
 	@BeforeMethod
 	public void BeforeTest() {
-		System.out.println("BeforeTest");
+		LOG.info("BeforeTest");
 	}
 	@AfterClass
 	public void AfterClass() {
-		System.out.println("Final");
+		LOG.info("Final");
 	}
 	@AfterMethod
 	public void AfterTest() {
-		System.out.println("AfterTest");
+		LOG.info("AfterTest");
 	}
 		
 	@DataProvider(name ="Authentication")
@@ -35,19 +39,19 @@ public class Example1Test {
 	
 	@Test(dataProvider = "Authentication")
 	public void test(String sUsername, String sPassword, String sName) {
-		System.out.println(sUsername);
-		System.out.println(sPassword);
-		System.out.println(sName);
+		LOG.info(sUsername);
+		LOG.info(sPassword);
+		LOG.info(sName);
 		Assert.assertEquals(sUsername, "testuser_1","Valor Username and testuser_1 are the same");
 		
 	}
 	
 	@Test
 	public void metodo1() {
-	System.out.println("Test1");
+		LOG.info("Test1");
 	}
 	@Test
 	public void metodo2() {
-	System.out.println("Test2");
+		LOG.info("Test2");
 	}
 }
