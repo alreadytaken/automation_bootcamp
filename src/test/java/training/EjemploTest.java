b2f2f650da8b131f7db97e5e863c63385578570c
cristianvaz30 <cristianvaz30@hotmail.com>
2018-03-13 21:14:26 -0300

    Guardando todo

--- src/test/java/training/EjemploTest.java
+++ src/test/java/training/EjemploTest.java
@@ -1,5 +1,7 @@
 package training;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -9,31 +11,41 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class EjemploTest {
+	private static final Logger LOG=LogManager.getLogger(EjemploTest.class);
+	
 	@Test
 	public void MetodoEjemplo() {
-		System.out.println("Metodoejemplo1 Test");
+		LOG.info("Metodoejemplo1 Test");
+		//System.out.println("Metodoejemplo1 Test");
 	}
 	@BeforeMethod
 	public void MetodoEjemplo2() {
-		System.out.println("Metodoejemplo2 BeforeMethod");
+		LOG.info("Metodoejemplo2 BeforeMethod");
+		//System.out.println("Metodoejemplo2 BeforeMethod");
 	}
 	@AfterMethod
 	public void MetodoEjemplo3() {
-		System.out.println("Metodoejemplo3 AfterMethod");
+		LOG.info("Metodoejemplo3 AfterMethod");
+		//System.out.println("Metodoejemplo3 AfterMethod");
 	}
 	@BeforeClass
 	public void MetodoEjemplo4() {
-		System.out.println("Metodoejemplo4 BeforeClass");
+		LOG.info("Metodoejemplo4 BeforeClass");
+		//System.out.println("Metodoejemplo4 BeforeClass");
 	}
 	@AfterClass
 	public void MetodoEjemplo5() {
-		System.out.println("Metodoejemplo5 AfterClass");
+		LOG.info("Metodoejemplo5 AfterClass");
+		//System.out.println("Metodoejemplo5 AfterClass");
 	}
 	@Test(dataProvider="Authentication")
 	public void MetodoEjemplo6(String sUsername,String sPassword,String sInfo) {
-		System.out.println(sUsername);
-		System.out.println(sPassword);
-		System.out.println(sInfo);
+		LOG.info(sUsername);
+		LOG.info(sPassword);
+		LOG.info(sInfo);
+		//System.out.println(sUsername);
+		//System.out.println(sPassword);
+		//System.out.println(sInfo);
 		Assert.assertEquals(sUsername,sInfo,"Verifico los nombres");
 		Assert.assertEquals(sUsername,sInfo,"2ndaverif");
 
@@ -44,7 +56,7 @@ public class EjemploTest {
 		System.out.println(sPassword);
 		System.out.println(sInfo);
 		Assert.assertEquals(sUsername,sInfo,"Verifico los nombres");
-		Assert.assertEquals(sUsername,sInfo,"2ndaverif");
+		Assert.assertEquals("a","b","2ndaverif");
 	}
 	@DataProvider(name="Authentication")
 	public Object[][] provider (){
@@ -52,3 +64,4 @@ public class EjemploTest {
 	}
 }
 
+ 
