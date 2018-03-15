8b076217ca292a8af7f0abd5deba18af38104b69
cristianvaz30 <cristianvaz30@hotmail.com>
2018-03-14 21:07:47 -0300

    da

--- src/test/java/training/EjemploTest.java
+++ src/test/java/training/EjemploTest.java
@@ -2,6 +2,11 @@ package training;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -10,29 +15,40 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
 public class EjemploTest {
 	private static final Logger LOG = LogManager.getLogger(EjemploTest.class);
-
+	private WebDriver driver;
 	@Test
 	public void MetodoEjemplo() {
 		LOG.info("Metodoejemplo1 Test");
+		WebElement txtUsername = driver.findElement(By.name("userName"));
+		//name=userName
+		//name=password
+		//name=login
 		// System.out.println("Metodoejemplo1 Test");
 	}
 
 	@BeforeMethod
 	public void MetodoEjemplo2() {
+		driver=new ChromeDriver();
 		LOG.info("Metodoejemplo2 BeforeMethod");
 		// System.out.println("Metodoejemplo2 BeforeMethod");
 	}
 
 	@AfterMethod
 	public void MetodoEjemplo3() {
+		//driver.quit();
 		LOG.info("Metodoejemplo3 AfterMethod");
 		// System.out.println("Metodoejemplo3 AfterMethod");
 	}
 
 	@BeforeClass
 	public void MetodoEjemplo4() {
+		LOG.info("Instalando Driver");
+		WebDriverManager.chromedriver().setup();
 		LOG.info("Metodoejemplo4 BeforeClass");
 		// System.out.println("Metodoejemplo4 BeforeClass");
 	}
