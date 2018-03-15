2507a6be5e90d73fd7a94320db83becd4079cad3
srodriguez47 <sebastianrodriguezpaiva@gmail.com>
2018-03-16 00:07:01 +0100

    googleEj

--- src/test/java/com/globant/automation/GoogleTest.java
+++ src/test/java/com/globant/automation/GoogleTest.java
@@ -3,12 +3,18 @@ package com.globant.automation;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
@@ -24,11 +30,26 @@ public class GoogleTest {
         LOG = LogManager.getLogger(GoogleTest.class);
     }
 
-    @Test
-    public void searchTest() throws InterruptedException {
+    
+    @DataProvider(name="Buscar")
+    public Object[][] provider(){
+    	return new Object [][] {{ "uno" },{"dos"},{"tres"}};
+    }
+    	
+    
+    
+    @Test (dataProvider="Buscar")
+    public void searchTest(String enviar)  {
         LOG.info("Search test");
         driver.get("https://google.com");
+        WebElement buscador =driver.findElement(By.name("q"));
+        buscador.click();
+        buscador.sendKeys(enviar);
+        buscador.sendKeys(Keys.ENTER);
+        WebElement e=(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(By.name("q")));
+        
     }
+    
 
     @BeforeMethod
     public void prepareTest() {
