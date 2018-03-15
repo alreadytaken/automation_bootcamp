ed21c4e9e6f69bd7efc71c5f51b715215464a0db
Santiago Caamaño <santicf@UY-IT00490.local>
2018-03-15 17:11:15 -0300

    Base project

---
+++ src/test/java/com/globant/automation/GoogleTest.java
@@ -0,0 +1,50 @@
+package com.globant.automation;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+
+    public GoogleTest() {
+
+    }
+
+    private static final Logger LOG;
+    private WebDriver driver;
+
+    static {
+        LOG = LogManager.getLogger(GoogleTest.class);
+    }
+
+    @Test
+    public void searchTest() throws InterruptedException {
+        LOG.error("Search test");
+        driver.get("https://google.com");
+        Thread.sleep(5000);
+    }
+
+    @BeforeMethod
+    public void prepareTest() {
+        LOG.info("Prepare test");
+        driver = new ChromeDriver();
+    }
+
+    @AfterMethod
+    public void cleanup() {
+        driver.quit();
+    }
+
+    @BeforeClass
+    private void prepareClass() {
+        LOG.info("Prepare class");
+        WebDriverManager.chromedriver().setup();
+    }
+}
---
+++ src/test/java/com/globant/automation/JUnitTest.java
@@ -0,0 +1,11 @@
+package com.globant.automation;
+
+import org.junit.Test;
+
+public class JUnitTest {
+
+    @Test
+    private void method() {
+
+    }
+}
