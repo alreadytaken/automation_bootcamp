45ef247e7a03897f4709697252f91fdc9df95ced
csilvalopez <camilasilvalopez@gmail.com>
2018-03-15 01:08:27 +0100

    coso

--- src/test/java/training/EjemploTest.java
+++ src/test/java/training/EjemploTest.java
@@ -1,7 +1,13 @@
 package training;
 
+
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -10,9 +16,18 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class EjemploTest {
 
+	private WebDriver driver;
+	 //name="userName" 
+	//name="password"
+	//name="login" (sign-in) 
+	
+	
+	
 private static final Logger LOG = LogManager.getLogger(EjemploTest.class);
 
 	@Test
@@ -24,20 +39,31 @@ private static final Logger LOG = LogManager.getLogger(EjemploTest.class);
 	@BeforeMethod
 	public void coso2() {
 		LOG.info("beforeTest");
+		driver = new FirefoxDriver(); //Crea una instancia del navegador
 	}
 	
 	@AfterMethod
 	public void coso3() {
 		LOG.info("aftertest");
+		driver.quit(); //Cierro el navegador
 	}
 
 	@Test
 	public void coso4() {
 		LOG.info("test");
+		WebElement txtUsername = driver.findElement(By.id("userName"));
+		txtUsername.click();
+		txtUsername.sendKeys("Pedro");
 	}
 @BeforeClass
 	public void coso5() {
 	LOG.info("test beforeClass");
+	
+	LOG.info("Instalando driver");
+	WebDriverManager.firefoxdriver().setup(); //asegura que tengo el driver de ff corriendo
+	
+
+	
 	}
 	@AfterClass
 	public void coso6() {
