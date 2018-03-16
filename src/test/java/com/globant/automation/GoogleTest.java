55f4445998a182eec25830206f8e8e1c3c69efda
Santiago Blanco <s.blancofretes@gmail.com>
2018-03-16 03:03:42 -0300

    GoogleTest funcional con dataProvider, SeleniumUtils funcional

--- src/test/java/com/globant/automation/GoogleTest.java
+++ src/test/java/com/globant/automation/GoogleTest.java
@@ -1,15 +1,18 @@
 package com.globant.automation;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class GoogleTest {
 
@@ -24,10 +27,20 @@ public class GoogleTest {
         LOG = LogManager.getLogger(GoogleTest.class);
     }
 
-    @Test
-    public void searchTest() throws InterruptedException {
-        LOG.info("Search test");
+	@DataProvider(name = "Search")
+	public Object[][] provider() {
+		return new Object[][] { { "Pink Floyd" }, { "Led Zeppelin" }, { "The Beatles" } };
+	}
+	
+    @Test(dataProvider = "Search")
+    public void searchTest(String search) throws InterruptedException {
+        LOG.info("Search test with dataProvider");
         driver.get("https://google.com");
+        WebElement searchBar = driver.findElement(By.name("q"));
+        searchBar.sendKeys(search);
+        searchBar.submit();
+        SeleniumUtils.waitUntilClickable(By.id("gsri_ok0"), driver);
+        
     }
 
     @BeforeMethod
