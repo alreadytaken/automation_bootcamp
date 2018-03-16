20ee28edadc72c74e00a9cbdaa4d5492440ceff7
nano02 <fcmont98@gmail.com>
2018-03-16 01:06:55 +0100

    dìa jueves

---
+++ src/main/java/com/globant/automation/SeleniumUtils.java
@@ -0,0 +1,19 @@
+package com.globant.automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SeleniumUtils {
+	public static WebElement waitUntilClickable(WebElement e, WebDriver driver) {
+		return (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(e));
+
+	}
+
+	public static WebElement waitUntilClickable(By by, WebDriver driver) {
+		return (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
+	}
+	
+}
--- src/test/java/com/globant/automation/GoogleTest.java
+++ src/test/java/com/globant/automation/GoogleTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -25,48 +26,49 @@ import org.testng.annotations.Test;
 
 public class GoogleTest {
 
-    public GoogleTest() {
- 
+	public GoogleTest() {
 
+	}
 
-    }
+	private static final Logger LOG;
+	private WebDriver driver;
 
-    private static final Logger LOG;
-    private WebDriver driver;
+	static {
+		LOG = LogManager.getLogger(GoogleTest.class);
+	}
 
-    static {
-        LOG = LogManager.getLogger(GoogleTest.class);
-    }
+	@Test(dataProvider = "Search")
+	public void searchTest(String Search) throws InterruptedException {
+		LOG.info("Search test");
+		driver.get("https://google.com");
+		WebElement txtSearch = SeleniumUtils.waitUntilClickable(By.name("q"), driver);
+		txtSearch.sendKeys(Search);
+		txtSearch.sendKeys(Keys.ENTER);
+		txtSearch.submit();
+		WebElement firstSearch = driver.findElements(By.xpath("//h3[@class=-'r'|/a");
+		//List<WebElement> resultados = driver;
+		//firstSearch.click();
+	}
 
-    @Test (dataProvider = "Search")
-    public void searchTest(String Search) throws InterruptedException {
-        LOG.info("Search test");
-        driver.get("https://google.com");
-        WebElement txtSearch = driver.findElement(By.name("q"));
-        txtSearch.sendKeys(Search);
-        txtSearch.sendKeys(Keys.ENTER);
-        txtSearch.submit();
-        WebElement e = (new WebDriverWait(driver,10)).until(ExpectedConditions.elementToBeClickable(By.name("q")));
-    }
+	@DataProvider(name = "Search")
+	public Object[][] provider() {
+		return new Object[][] { { "autos" }, { "computadoras" }, { "amazon" } };
+	}
 
-    @DataProvider (name = "Search") 
-    public Object [][] provider(){
-    	return new Object [][] {{"autos"},{"computadoras"},{"amazon"}};
-    }
-    @BeforeMethod
-    public void prepareTest() throws MalformedURLException {
-        LOG.info("Prepare test");
-        driver = new ChromeDriver();
-    }
+	@BeforeMethod
+	public void prepareTest() throws MalformedURLException {
+		LOG.info("Prepare test");
+		driver = new ChromeDriver();
+	}
 
-    @AfterMethod
-    public void cleanup() {
-        driver.quit();
-    }
+	@AfterMethod
+	public void cleanup() {
+		driver.quit();
+	}
 
-    @BeforeClass
-    private void prepareClass() {
-        LOG.info("Prepare class");
-        WebDriverManager.chromedriver().setup();
-    }
+	@BeforeClass
+	private void prepareClass() {
+		LOG.info("Prepare class");
+		WebDriverManager.chromedriver().setup();
+	}
 }
