53feed5395aca151efc5aaf614d1ac6ddc165c2a
BernardoGO95 <bernardogo95@hotmail.com>
2018-03-16 01:08:15 +0100

    clase que no entendi mucho 15/03/18

---
+++ src/main/java/com/globant/automation/SeleniumUtils.java
@@ -0,0 +1,23 @@
+package com.globant.automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class SeleniumUtils {
+	
+	public static WebElement waitUntilClickable(WebElement e, WebDriver driver) {
+		return new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable(By.name("q")));
+		
+		
+	}
+	public static WebElement waitUntilClickable(By by , WebDriver driver) {
+		return new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable(By.name("q")));
+		
+		
+	}
+
+}
