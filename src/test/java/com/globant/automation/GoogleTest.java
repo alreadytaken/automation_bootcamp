633007bdf9533e4c2c1c5f704d0a4447c24feea7
csilvalopez <camilasilvalopez@gmail.com>
2018-03-16 01:10:12 +0100

    15-3

--- src/test/java/com/globant/automation/GoogleTest.java
+++ src/test/java/com/globant/automation/GoogleTest.java
@@ -1,49 +1,95 @@
 package com.globant.automation;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.sql.Driver;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class GoogleTest {
 
-    public GoogleTest() {
+	public GoogleTest() {
 
-    }
+	}
 
-    private static final Logger LOG;
-    private WebDriver driver;
+	private static final Logger LOG;
+	private WebDriver driver;
+	//@FindBy(name="q")
+	//private WebElement txtsearch;
+	
+	//@FindBy(name="userName")
+	//private WebElement userMT;
+	
+	static {
+		LOG = LogManager.getLogger(GoogleTest.class);
+	}
 
-    static {
-        LOG = LogManager.getLogger(GoogleTest.class);
-    }
+	@Test
+	public void searchTest() throws InterruptedException {
+		LOG.info("Search test");
+		driver.get("http://newtours.demoaut.com");
+		
+		//WebElement txtsearch = driver.findElement(By.name("q"));
+		//txtsearch.sendKeys("amazon");
+		//txtsearch.sendKeys(Keys.ENTER);
+		
+		//WebElement gmail = driver.findElement(By.className("gb_P"));
+		
+		//Glorioso Mercury tours
+		WebElement userMT = (new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(By.name("userName")));
+		userMT.sendKeys("Camila");
+		
+		WebElement passMT =(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(By.name("password")));
+		passMT.sendKeys("Camila");
+		passMT.sendKeys(Keys.ENTER);
+		
+		driver.get("www.google.com");
+		
+		WebElement txtsearch = SeleniumUtils.waitUntilClickeable(By.name("q"), driver);
+		
+		List <WebElement> resultados = driver.findElements(By.xpath("//div[@class='rc']/h3/a"));
+		LOG.info(resultados.toString());
+		
+		
+	}
 
-    @Test
-    public void searchTest() throws InterruptedException {
-        LOG.info("Search test");
-        driver.get("https://google.com");
-    }
+	@BeforeMethod
+	public void prepareTest() throws MalformedURLException {
+		LOG.info("Prepare test");
+		//DesiredCapabilities caps = DesiredCapabilities.firefox();
+		//driver = new RemoteWebDriver(new URL("http://127.0.0.1:4444/wd/hub"), caps);
+		driver = new ChromeDriver();
+	}
 
-    @BeforeMethod
-    public void prepareTest() {
-        LOG.info("Prepare test");
-        driver = new ChromeDriver();
-    }
+	@AfterMethod
+	public void cleanup() {
+		driver.quit();
+	}
 
-    @AfterMethod
-    public void cleanup() {
-        driver.quit();
-    }
-
-    @BeforeClass
-    private void prepareClass() {
-        LOG.info("Prepare class");
-        WebDriverManager.chromedriver().setup();
-    }
+	@BeforeClass
+	private void prepareClass() {
+		LOG.info("Prepare class");
+		WebDriverManager.chromedriver().setup();
+	}
 }
