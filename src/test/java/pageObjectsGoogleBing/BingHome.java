17052b5a7d6b86d309cb6babe258f1d08c8b140b
Santiago Blanco <s.blancofretes@gmail.com>
2018-03-18 16:14:16 -0300

    Google Vs Bing implemented in POM

---
+++ src/test/java/pageObjectsGoogleBing/BingHome.java
@@ -0,0 +1,29 @@
+package pageObjectsGoogleBing;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+import com.globant.automation.SeleniumUtils;
+
+public class BingHome {
+	protected WebDriver driver;
+	
+	public BingHome(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		SeleniumUtils.waitUntilClickable(By.id("sb_form_go"), driver);
+	}
+	
+	@FindBy(how = How.ID, using = "sb_form_q")
+	private WebElement searchBar;
+	
+	public BingResults searchQuery(String search) {
+		searchBar.sendKeys(search);
+		searchBar.submit();
+		return new BingResults(driver);
+	}
+}
