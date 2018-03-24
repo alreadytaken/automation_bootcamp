403f4efd27e035a258ec544ce8f4f2938d1cae50
abrandon26 <brandon.alvaro2017@gmail.com>
2018-03-23 21:18:34 -0300

    me mataron los xpath

--- src/test/java/com/globant/automation/ProductPage.java
+++ src/test/java/com/globant/automation/ProductPage.java
@@ -8,11 +8,13 @@ import org.openqa.selenium.support.How;
 public class ProductPage extends AmazonHomePage{
 	
 
-	@FindBy(how = How.ID, using = "")
+	@FindBy(how = How.XPATH, using = "//*[@id='customer_review-R5EI4C8P5MRZI']/div[4]/span/div/div[1]")
 	private WebElement review;
 	
 	public ProductPage (WebDriver driver) {
 	    super(driver);
 }
+	
+	
 
 }
